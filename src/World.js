01f1f1105b3e84d780d0eea1f209ffba6b08e0ac
Jamie Nichols <jamie337nichols@gmail.com>
2023-09-07 14:11:15 -0400

    fix: integrating rimraf in package.json

---
+++ src/World.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var World = /** @class */ (function () {
+    function World() {
+    }
+    return World;
+}());
+exports.default = World;
